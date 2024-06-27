b657d549a2654d8c87ef1c0b3b2109cad42f1c74
AmirReza Saeedi <ars86677@gmail.com>
2024-06-28 00:49:22 +0330

    Update TB.v

--- Code/TB.v
+++ Code/TB.v
@@ -38,41 +38,44 @@ initial begin
     is_uni_car_exited = 1;
 
     // tb1:
-    for (i = 0; i < 1000; i = i + 1) begin
-        #1 car_entered <= !car_entered;
-    end
-    #1000 $stop;
+    // for (i = 0; i < 1000; i = i + 1) begin
+    //     #1 car_entered <= !car_entered;
+    // end
+    // #1000 $stop;
 
     // tb2
-    // for (i = 0; i < 600; i = i + 1) begin
-    //     #1 car_entered <= !car_entered;
-    // end
+    for (i = 0; i < 600; i = i + 1) begin
+        #1 car_entered <= !car_entered;
+    end
 
 
-    // for (i = 0; i < 600; i = i + 1) begin
-    //     #1 car_exited <= !car_exited;
-    // end
+    for (i = 0; i < 600; i = i + 1) begin
+        #1 car_exited <= !car_exited;
+    end
 
-    // #2000
-    // for (i = 0; i < 1200; i = i + 1) begin
-    //     #1 car_entered <= !car_entered;
-    // end
+    #2000
+    for (i = 0; i < 1200; i = i + 1) begin
+        #1 car_entered <= !car_entered;
+    end
 
-    // is_uni_car_entered = 0;
-    // for (i = 0; i < 600; i = i + 1) begin
-    //     #1 car_entered <= !car_entered;
-    // end
+    is_uni_car_entered = 0;
+    for (i = 0; i < 600; i = i + 1) begin
+        #1 car_entered <= !car_entered;
+    end
 
-    // #600
-    // for (i = 0; i < 100; i = i + 1) begin
-    //     #1 car_entered <= !car_entered;
-    //     car_exited <= !car_exited;
-    // end
+    #600
+    for (i = 0; i < 100; i = i + 1) begin
+        #1 car_entered <= !car_entered;
+    end
+    
+    for (i = 0; i < 100; i = i + 1) begin
+        #1 car_exited <= !car_exited;
+    end
 
-    // for (i = 0; i < 100; i = i + 1) begin
-    //     #1 car_entered <= !car_entered;
-    // end
+    for (i = 0; i < 100; i = i + 1) begin
+        #1 car_entered <= !car_entered;
+    end
 
-    // #1000 $stop();
+    #1000 $stop();
 end
 endmodule
